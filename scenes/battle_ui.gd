9a554996f3a9cec1a5b22896a9941d14f98961fb
xuehai3-cyber <xuehai3-cyber@users.noreply.github.com>
2026-08-07 22:01:08 +0800

    修复字体加载方式：FontFile + font_data

--- scenes/battle_ui.gd
+++ scenes/battle_ui.gd
@@ -80,10 +80,12 @@ func _ready() -> void:
 	label.text = "按空格开始\n%s" % _hp_text()
 
 	# 加载中文字体（网页版需要）
-	var cjk_font := load("res://assets/simhei.ttf") as Font
-	label.add_theme_font_override("font", cjk_font)
-	atk_btn.add_theme_font_override("font", cjk_font)
-	blk_btn.add_theme_font_override("font", cjk_font)
+	var ff := FontFile.new()
+	ff.font_data = load("res://assets/simhei.ttf")
+	ff.fixed_size = LABEL_FONT_SIZE
+	label.add_theme_font_override("font", ff)
+	atk_btn.add_theme_font_override("font", ff)
+	blk_btn.add_theme_font_override("font", ff)
 
 	_create_hp_bars()
 
